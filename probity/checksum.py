44e0f5397dc4ad6d0ebb8084ca604cbd340babde
Alex Morega <alex@grep.ro>
2009-11-14 20:29:38 +0200

    folder listing with checksum of files

--- probity/checksum.py
+++ probity/checksum.py
@@ -1,4 +1,7 @@
 from hashlib import sha1
+from StringIO import StringIO
+import os
+from os import path
 
 def file_sha1(file_path):
     sha1_hash = sha1()
@@ -9,3 +12,13 @@ def file_sha1(file_path):
                 break
             sha1_hash.update(data)
     return sha1_hash.hexdigest()
+
+def folder_sha1(folder_path):
+    out = StringIO()
+    folder_name = path.basename(folder_path)
+    print>>out, '[begin folder "%s"]' % folder_name
+    for file_name in sorted(os.listdir(folder_path)):
+        file_checksum = file_sha1(path.join(folder_path, file_name))
+        print>>out, '%s:' % file_name, file_checksum
+    print>>out, '[end folder "%s"]' % folder_name
+    return out.getvalue()
--- probity/tests/test_checksums.py
+++ probity/tests/test_checksums.py
@@ -1,6 +1,7 @@
 import unittest
 import tempfile
 import shutil
+import os
 from os import path
 
 from probity import checksum
@@ -33,3 +34,23 @@ class ChecksumsTestCase(unittest.TestCase):
                 f.write('asdf' * 10)
         self.assertEqual(checksum.file_sha1(testfile_path),
                          'b3c791bfc591d5c7514c135465a484e4a0a3ae85')
+
+    def test_folder_sha1(self):
+        testfolder_path = path.join(self.tmpdir, 'testfolder')
+        os.mkdir(testfolder_path)
+        with open(path.join(testfolder_path, 'file1'), 'wb') as f:
+            pass
+
+        with open(path.join(testfolder_path, 'file2'), 'wb') as f:
+            f.write('hello probity!')
+
+        with open(path.join(testfolder_path, 'file3'), 'wb') as f:
+            for c in xrange(1000):
+                f.write('asdf' * 10)
+
+        self.assertEqual(checksum.folder_sha1(testfolder_path),
+                         ('[begin folder "testfolder"]\n'
+                          'file1: da39a3ee5e6b4b0d3255bfef95601890afd80709\n'
+                          'file2: 6e28214b93900151eda8143c5605a5d084ee165c\n'
+                          'file3: b3c791bfc591d5c7514c135465a484e4a0a3ae85\n'
+                          '[end folder "testfolder"]\n'))
