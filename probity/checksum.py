744a151ccf1c6cab84f47e36aa2db1f5135a7ad4
Alex Morega <alex@grep.ro>
2009-12-23 16:22:52 +0200

    changed default read size to 64KB

--- probity/checksum.py
+++ probity/checksum.py
@@ -6,7 +6,7 @@ def file_sha1(file_path):
     sha1_hash = sha1()
     with open(file_path, 'rb') as f:
         while True:
-            data = f.read(4096)
+            data = f.read(65536)
             if not data:
                 break
             sha1_hash.update(data)
