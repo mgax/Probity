ab8750588099c0b82a071c5386ef1c80aef287da
Alex Morega <alex@grep.ro>
2009-12-24 20:16:26 +0200

    renamed test folder in script tests

--- probity/tests/test_script.py
+++ probity/tests/test_script.py
@@ -32,11 +32,10 @@ def invoke_script(args):
 class InvokeScriptTestCase(unittest.TestCase):
     def setUp(self):
         self.tmpdir = tempfile.mkdtemp()
-        testfolder_path = path.join(self.tmpdir, 'testfolder')
-        os.mkdir(testfolder_path)
-        os.mkdir(path.join(testfolder_path, 'sub1'))
-        os.mkdir(path.join(testfolder_path, 'sub1/sub2'))
-        with open(path.join(testfolder_path, 'sub1/sub2/file1'), 'wb') as f:
+        os.mkdir(path.join(self.tmpdir, 'testf'))
+        os.mkdir(path.join(self.tmpdir, 'testf/sub1'))
+        os.mkdir(path.join(self.tmpdir, 'testf/sub1/sub2'))
+        with open(path.join(self.tmpdir, 'testf/sub1/sub2/file1'), 'wb') as f:
             f.write('hello probity!')
 
     def tearDown(self):
@@ -48,23 +47,23 @@ class InvokeScriptTestCase(unittest.TestCase):
         self.assertTrue('Usage: probity' in out, (out,err))
 
     def test_checksum_file(self):
-        file_path = path.join(self.tmpdir, 'testfolder/sub1/sub2/file1')
+        file_path = path.join(self.tmpdir, 'testf/sub1/sub2/file1')
         out, err = invoke_script([file_path])
         self.assertEqual(err, '')
         self.assertEqual(out, 'file1: 6e28214b93900151eda8'
                                      '143c5605a5d084ee165c\n')
 
     def test_checksum_folder(self):
-        out, err = invoke_script([path.join(self.tmpdir, 'testfolder')])
+        out, err = invoke_script([path.join(self.tmpdir, 'testf')])
         self.assertEqual(err, '')
-        self.assertEqual(out, 'testfolder: 47c14f38141d8fcb6e22'
+        self.assertEqual(out, 'testf: 47c14f38141d8fcb6e22'
                                           '09fbe990a7ddc102b2b2\n')
 
     def test_verbose(self):
-        out, err = invoke_script(['-v', path.join(self.tmpdir, 'testfolder')])
+        out, err = invoke_script(['-v', path.join(self.tmpdir, 'testf')])
         self.assertEqual(err, '')
         self.assertEqual(out,
-                         ('[begin folder "testfolder"]\n'
+                         ('[begin folder "testf"]\n'
                           '[begin folder "sub1"]\n'
                           '[begin folder "sub2"]\n'
                           'file1: 6e28214b93900151eda8143c5605a5d084ee165c\n'
@@ -72,5 +71,5 @@ class InvokeScriptTestCase(unittest.TestCase):
                               '24c89cdebe9328b35a4afc43cdcbe5e38ab64c06]\n'
                           '[end folder "sub1": '
                               '55322dc65ff9ece08af84199b73000d3a9d80fa0]\n'
-                          '[end folder "testfolder": '
+                          '[end folder "testf": '
                               '47c14f38141d8fcb6e2209fbe990a7ddc102b2b2]\n'))
