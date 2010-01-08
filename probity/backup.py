fc04c37c73b7cc6cdb4ef68230f772ab7f862bd7
Alex Morega <alex@grep.ro>
2010-01-08 20:17:37 +0200

    use temporary files when doing backup

--- probity/backup.py
+++ probity/backup.py
@@ -1,5 +1,6 @@
 import os
 from os import path
+import tempfile
 
 from probity.walk import BLOCK_SIZE
 
@@ -12,18 +13,20 @@ class Backup(object):
             return
 
         bucket_path = path.join(self.pool_path, evt.checksum[:2])
-        file_path = path.join(bucket_path, evt.checksum[2:])
+        blob_path = path.join(bucket_path, evt.checksum[2:])
 
         if not path.isdir(bucket_path):
             os.makedirs(bucket_path)
 
-        if not path.isfile(file_path):
+        if not path.isfile(blob_path):
             # TODO: use temporary file!
-            with open(evt.fs_path, 'rb') as src_file:
-                with open(file_path, 'wb') as dst_file:
+            temp_path = tempfile.mkstemp(dir=self.pool_path)[1]
+            with open(temp_path, 'wb') as temp_file:
+                with open(evt.fs_path, 'rb') as src_file:
                     while True:
                         data = src_file.read(BLOCK_SIZE)
                         if data:
-                            dst_file.write(data)
+                            temp_file.write(data)
                         else:
                             break
+            os.rename(temp_path, blob_path)
