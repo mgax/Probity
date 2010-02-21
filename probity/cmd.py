cf05da151ca39c90f5d879eb5713c8aef60a0de4
Alex Morega <alex@grep.ro>
2010-02-21 13:50:36 +0100

    use command-line sub-arguments

--- probity/cmd.py
+++ probity/cmd.py
@@ -8,60 +8,74 @@ from probity import backup
 
 def parse_probity_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("target", nargs="+",
-        help="folder or file to be checksummed")
-    parser.add_argument("-q", "--quiet",
+    subparsers = parser.add_subparsers(dest='subcmd')
+
+    checksum_parser = subparsers.add_parser('checksum',
+        help="calculate the checksum of a folder")
+    checksum_parser.add_argument("target", help="folder to be checksummed")
+    checksum_parser.add_argument("-q", "--quiet",
         action="store_true", dest="quiet", default=False,
         help="only print final checksum")
-    parser.add_argument("-r", "--reference",
-        action="store", dest="reference_path", metavar="REFERENCE_FILE",
-        help="verify against previous report at REFERENCE_FILE")
-    parser.add_argument("-b", "--backup",
+
+    backup_parser = subparsers.add_parser('backup',
+        help="Back up a folder to a hashed filesystem repository")
+    backup_parser.add_argument("target", help="folder to be checksummed")
+    backup_parser.add_argument("-b", "--backup",
         action="store", dest="backup_path", metavar="BACKUP_PATH",
         help="back up files to BACKUP_PATH")
-    parser.add_argument("-i", "--interactive",
-        action="store_true", dest="interactive", default=False,
+
+    verify_parser = subparsers.add_parser('verify',
+        help="Verify the contents of a given folder against a report file")
+    verify_parser.add_argument("target", help="folder to be checksummed")
+    verify_parser.add_argument("-r", "--reference",
+        action="store", dest="reference_path", metavar="REFERENCE_FILE",
+        help="verify against previous report at REFERENCE_FILE")
+
+    interact_parser = subparsers.add_parser('interact',
         help="start a read-eval-print loop")
+
     return parser.parse_args()
 
 def main():
     args = parse_probity_args()
 
-    if args.reference_path is not None:
+    if args.subcmd == 'checksum':
+        do_checksum(args.target, print_all=(not args.quiet))
+
+    elif args.subcmd == 'backup':
+        do_checksum(args.target, backup_pool=backup.Backup(args.backup_path))
+
+    elif args.subcmd == 'verify':
         reference = read_checksum_file(args.reference_path)
         comparator = compare.Comparator(reference)
-    else:
-        comparator = None
-
-    if args.backup_path is not None:
-        backup_pool = backup.Backup(args.backup_path)
-    else:
-        backup_pool = None
-
-    if args.interactive:
-        import code
-        return code.interact("Probity interactive session", local=locals())
-
-    for item in args.target:
-        base_path, root_name = path.split(item)
-        for evt in walk.walk_item(base_path, root_name):
-            if not args.quiet:
-                sys.stdout.write(str(evt))
-            if comparator is not None:
-                comparator.update(evt)
-            if backup_pool is not None:
-                backup_pool.store(evt)
-
-        if args.quiet:
-            root_checksum = evt.checksum
-            print '%s: %s' % (root_name, root_checksum)
-
-    if comparator is not None:
+        do_checksum(args.target, comparator=comparator)
         report = comparator.report()
         if report['missing']:
             print 'Missing files:'
             for file_path in report['missing']:
                 print '  %s: %s' % (file_path, reference[file_path])
+
+    elif args.subcmd == 'interact':
+        import code
+        return code.interact("Probity interactive session", local=locals())
+
+    else:
+        raise NotImplementedError
+
+def do_checksum(target_path, print_all=False,
+                backup_pool=None, comparator=None):
+    base_path, root_name = path.split(target_path)
+    for evt in walk.walk_item(base_path, root_name):
+        if print_all:
+            sys.stdout.write(str(evt))
+        if comparator is not None:
+            comparator.update(evt)
+        if backup_pool is not None:
+            backup_pool.store(evt)
+
+    if not print_all:
+        root_checksum = evt.checksum
+        print '%s: %s' % (root_name, root_checksum)
 
 def read_checksum_file(file_path):
     data = dict()
--- probity/tests/test_script.py
+++ probity/tests/test_script.py
@@ -48,19 +48,20 @@ class InvokeScriptTestCase(unittest.TestCase):
 
     def test_checksum_file(self):
         file_path = path.join(self.tmpdir, 'testf/sub1/sub2/file1')
-        out, err = invoke_script([file_path])
+        out, err = invoke_script(['checksum', file_path])
         self.assertEqual(err, '')
         self.assertEqual(out, 'file1: 6e28214b93900151eda8'
                                      '143c5605a5d084ee165c\n')
 
     def test_checksum_folder(self):
-        out, err = invoke_script([path.join(self.tmpdir, 'testf'), '-q'])
+        out, err = invoke_script(['checksum', '-q',
+                                  path.join(self.tmpdir, 'testf')])
         self.assertEqual(err, '')
         self.assertEqual(out, 'testf: 47c14f38141d8fcb6e22'
                                           '09fbe990a7ddc102b2b2\n')
 
     def test_verbose(self):
-        out, err = invoke_script([path.join(self.tmpdir, 'testf')])
+        out, err = invoke_script(['checksum', path.join(self.tmpdir, 'testf')])
         self.assertEqual(err, '')
         self.assertEqual(out,
                          ('[begin folder "testf"]\n'
@@ -83,15 +84,16 @@ class InvokeScriptTestCase(unittest.TestCase):
                         'e897d8148ce4562021397535ae72f7d3b6752fa1]\n')
         testf_path = path.join(self.tmpdir, 'testf')
         old_prob_path = path.join(self.tmpdir, 'old.prob')
-        out, err = invoke_script([testf_path, '--reference=' + old_prob_path])
+        out, err = invoke_script(['verify', testf_path,
+                                  '--reference=' + old_prob_path])
         self.assertEqual(err, '')
         expected_out = ('Missing files:\n'
                         '  old/file_two: '
                             '92f3bd369c2639c30e97d5163d4a8693928c411e\n')
-        self.assertTrue(expected_out in out)
+        self.assertTrue(expected_out in out, out)
 
     def test_backup(self):
-        out, err = invoke_script([path.join(self.tmpdir, 'testf'), '-q',
+        out, err = invoke_script(['backup', path.join(self.tmpdir, 'testf'),
                                   '-b', path.join(self.tmpdir, 'backup')])
         self.assertEqual(err, '')
         self.assertEqual(out, 'testf: 47c14f38141d8fcb6e22'
